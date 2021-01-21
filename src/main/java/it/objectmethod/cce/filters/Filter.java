74782fbb97fb9cc3086f97c2d8e284122b1dc265
Sean <sean.giovine@objectmethod.it>
2021-01-21 12:57:31 +0100

    Filter joined project

---
+++ src/main/java/it/objectmethod/cce/filters/Filter.java
@@ -0,0 +1,55 @@
+package it.objectmethod.cce.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Filter implements javax.servlet.Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		String token = req.getHeader("token");
+		String url = req.getRequestURI();
+		boolean allow = false;
+		
+		System.out.println(url);
+		System.out.println(token);
+
+		if (!url.equals("/login")) {
+			System.out.println("diverso da login");
+			if ("1".equals(token)) {
+				System.out.println("token = 1");
+				allow = true;
+			} else {
+				System.out.println("token != 1");
+				allow = false;
+			}
+		} else {
+			System.out.println("percorso login");
+			allow = true;
+		}
+		
+		System.out.println(allow);
+		if (allow) {
+			chain.doFilter(request, response);
+		} else {
+			resp.setStatus(HttpStatus.UNAUTHORIZED.value());
+		}
+		System.out.println("--------------------------------------");
+	}
+
+}
